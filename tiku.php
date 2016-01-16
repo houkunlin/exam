f4c8aa1f9995e0a07c85de813fe54efdbd484b44
1184511588@qq.com <1184511588@qq.com>
2016-01-16 22:59:53 +0800

    全部添加

---
+++ tiku.php
@@ -0,0 +1,170 @@
+<?php
+
+/*
+1. rewarding
+2. communicate
+3. access
+4. embarrassing
+5. positive
+6. commitment
+7. virtual
+array(
+timu题目
+kong给的词
+ok正确填空内容
+);
+*/
+
+$array=array(
+array(
+'timu'=>'The years he sepent in the coutrtryside proved to be a _______ experience. ',
+'kong'=>'',
+'ok'=>'rewarding'),
+
+
+array(
+'timu'=>'You can learn a lot from this online course. It is designed to help people ______ better through speech and writing.',
+'kong'=>'',
+'ok'=>'communicate'),
+
+
+array(
+'timu'=>'Over a third of the population was estimated to have no ______ to he alth service. ',
+'kong'=>'',
+'ok'=>'access'),
+
+
+array(
+'timu'=>'Asking too many personal questions during an interview can lead to a(n) ______ situation.',
+'kong'=>'',
+'ok'=>'embarrassing'),
+
+
+
+array(
+'timu'=>'Don't just complain about what's wrong with it ; suggest some ______ ways to solve the problem.',
+'kong'=>'',
+'ok'=>'positive'),
+
+
+
+array(
+'timu'=>'No one in the class could match johns hard work and ______ to study, which is why the problem.',
+'kong'=>'',
+'ok'=>'commitment'),
+
+
+
+array(
+'timu'=>'The Web site allows you to take a(n) ______ tour of the well-known city which was there about 2, 000 years ago.',
+'kong'=>'',
+'ok'=>'virtual'),
+/*
+1. appeals
+2. identity
+3. concern
+4. disgusting
+5. upset
+6. influence
+7. offensive
+*/
+
+
+array(
+'timu'=>'If the bike ______ to you that much, why not buy it? You just got paid yesterday. ',
+'kong'=>'',
+'ok'=>'appeals'),
+
+
+array(
+'timu'=>'He managed to hide his ______ and moved to a new town where people would not know him.',
+'kong'=>'',
+'ok'=>'identity'),
+
+
+array(
+'timu'=>'All of your ______ was for nothing because I was safe all night.',
+'kong'=>'',
+'ok'=>'concern'),
+
+
+array(
+'timu'=>'It was worse than horrible ; the smell in the room was so ______ that it actually made me sick.',
+'kong'=>'',
+'ok'=>'disgusting'),
+
+
+array(
+'timu'=>'I wasn't quite angry, but I was a little ______ when my noodles were cooked poorly.',
+'kong'=>'',
+'ok'=>'upset'),
+
+
+array(
+'timu'=>'Modern music may have a neative influence as well as a ______ one on teenagers.',
+'kong'=>'',
+'ok'=>'positive'),
+
+
+array(
+'timu'=>'He has made a(n) ______ remark that angers all of us.',
+'kong'=>'',
+'ok'=>'offensive'),
+
+/*
+1. inwardly
+2. regret
+3. occasion
+4. complained
+5. urged
+6. coordinate
+7. reluctance
+*/
+
+array(
+'timu'=>'______ I was very unhappy with what he had said, but ı didn't show it.',
+'kong'=>'',
+'ok'=>'Inwardly'),
+
+
+array(
+'timu'=>'I deeply ______ to inform you that we cannot accept such conditions. ',
+'kong'=>'',
+'ok'=>'regret'),
+
+
+array(
+'timu'=>'He translated books not only from English, but also, on ______,from French.',
+'kong'=>'',
+'ok'=>'occasion'),
+
+
+array(
+'timu'=>'The young couple ______ about the high cost of visiting Europe.',
+'kong'=>'',
+'ok'=>'complained'),
+
+
+array(
+'timu'=>'He had repeatedly ______ her to come to the United States to join him, but she refused to leave her homeland.',
+'kong'=>'',
+'ok'=>'urged'),
+
+
+array(
+'timu'=>'Government officials visited the flood area on Thursday morning to ______ the relief efforts.',
+'kong'=>'',
+'ok'=>'coordinate'),
+
+
+array(
+'timu'=>'He showed great ______ in signing the agreement, but he finally did it.',
+'kong'=>'',
+'ok'=>'reluctance')
+
+);
+
+
+
+
+?>
