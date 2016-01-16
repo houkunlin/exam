e8f6b79b8fabede7832985933c9440718f49c184
1184511588@qq.com <1184511588@qq.com>
2016-01-16 23:18:12 +0800

    添加主页，修复题库

--- tiku.php
+++ tiku.php
@@ -14,50 +14,34 @@ kong给的词
 ok正确填空内容
 );
 */
-
-$array=array(
+$tiku=array(
 array(
 'timu'=>'The years he sepent in the coutrtryside proved to be a(n) _______ experience. ',
-'kong'=>'',
+'kong'=>'reward',
 'ok'=>'rewarding'),
-
-
 array(
 'timu'=>'You can learn a lot from this online course. It is designed to help people ______ better through speech and writing.',
-'kong'=>'',
+'kong'=>'communicate',
 'ok'=>'communicate'),
-
-
 array(
 'timu'=>'Over a third of the population was estimated to have no ______ to he alth service. ',
-'kong'=>'',
+'kong'=>'access',
 'ok'=>'access'),
-
-
 array(
 'timu'=>'Asking too many personal questions during an interview can lead to a(n) ______ situation.',
-'kong'=>'',
+'kong'=>'embarrass',
 'ok'=>'embarrassing'),
-
-
-
 array(
 'timu'=>'Don't just complain about what's wrong with it ; suggest some ______ ways to solve the problem.',
-'kong'=>'',
+'kong'=>'positive',
 'ok'=>'positive'),
-
-
-
 array(
 'timu'=>'No one in the class could match johns hard work and ______ to study, which is why the problem.',
-'kong'=>'',
+'kong'=>'commitment',
 'ok'=>'commitment'),
-
-
-
 array(
 'timu'=>'The Web site allows you to take a(n) ______ tour of the well-known city which was there about 2, 000 years ago.',
-'kong'=>'',
+'kong'=>'virtual',
 'ok'=>'virtual'),
 /*
 1. appeals
@@ -68,49 +52,34 @@ array(
 6. influence
 7. offensive
 */
-
-
 array(
 'timu'=>'If the bike ______ to you that much, why not buy it? You just got paid yesterday. ',
-'kong'=>'',
+'kong'=>'appeal',
 'ok'=>'appeals'),
-
-
 array(
 'timu'=>'He managed to hide his ______ and moved to a new town where people would not know him.',
-'kong'=>'',
+'kong'=>'identity',
 'ok'=>'identity'),
-
-
 array(
 'timu'=>'All of your ______ was for nothing because I was safe all night.',
-'kong'=>'',
+'kong'=>'concern',
 'ok'=>'concern'),
-
-
 array(
 'timu'=>'It was worse than horrible ; the smell in the room was so ______ that it actually made me sick.',
-'kong'=>'',
+'kong'=>'disgusting',
 'ok'=>'disgusting'),
-
-
 array(
 'timu'=>'I wasn't quite angry, but I was a little ______ when my noodles were cooked poorly.',
-'kong'=>'',
+'kong'=>'upset',
 'ok'=>'upset'),
-
-
 array(
 'timu'=>'Modern music may have a neative ______ as well as a positive one on teenagers.',
-'kong'=>'',
+'kong'=>'influence',
 'ok'=>'influence'),
-
-
 array(
 'timu'=>'He has made a(n) ______ remark that angers all of us.',
-'kong'=>'',
+'kong'=>'offensive',
 'ok'=>'offensive'),
-
 /*
 1. inwardly
 2. regret
@@ -120,48 +89,34 @@ array(
 6. coordinate
 7. reluctance
 */
-
 array(
 'timu'=>'______ I was very unhappy with what he had said, but ı didn't show it.',
-'kong'=>'',
+'kong'=>'inwardly',
 'ok'=>'Inwardly'),
-
-
 array(
 'timu'=>'I deeply ______ to inform you that we cannot accept such conditions. ',
-'kong'=>'',
+'kong'=>'regret',
 'ok'=>'regret'),
-
-
 array(
 'timu'=>'He translated books not only from English, but also, on ______,from French.',
-'kong'=>'',
+'kong'=>'occasion',
 'ok'=>'occasion'),
-
-
 array(
 'timu'=>'The young couple ______ about the high cost of visiting Europe.',
-'kong'=>'',
+'kong'=>'complain',
 'ok'=>'complained'),
-
-
 array(
 'timu'=>'He had repeatedly ______ her to come to the United States to join him, but she refused to leave her homeland.',
-'kong'=>'',
+'kong'=>'urge',
 'ok'=>'urged'),
-
-
 array(
 'timu'=>'Government officials visited the flood area on Thursday morning to ______ the relief efforts.',
-'kong'=>'',
+'kong'=>'coordinate',
 'ok'=>'coordinate'),
-
-
 array(
 'timu'=>'He showed great ______ in signing the agreement, but he finally did it.',
-'kong'=>'',
+'kong'=>'reluctance',
 'ok'=>'reluctance')
-
 );
 
 
